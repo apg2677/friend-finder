bfce90e9b2596f376e5c4b5d38962dda497f7e07
Amir P <apourrezaei@gmail.com>
2019-05-01 17:39:50 -0400

    friends

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = 
+[
+    {
+        name: "Angel",
+        scores: [1,2,3,4,5,5,4,2,1,1]
+    },
+    {
+        name: "Phil",
+        scores: [3,2,1,4,5,1,2,3,3,1]
+    },
+    {
+        name: "Jack",
+        scores: [5,4,3,2,5,5,5,3,3,2]
+    },
+    {
+        name: "Kim",
+        scores: [3,2,3,2,2,3,4,5,5,5]
+    },
+    {
+        name: "Pat",
+        scores: [3,2,3,3,5,3,3,3,3,1]
+    }
+
+];
+
+module.exports = friends;
+
