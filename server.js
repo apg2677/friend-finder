17e75e1360115334171a33feba257d36bdd7d58a
Amir P <apourrezaei@gmail.com>
2019-05-01 17:17:45 -0400

    Html route - survey

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+
+
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
