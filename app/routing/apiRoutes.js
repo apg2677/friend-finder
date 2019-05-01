17efa82456f64e9159a963685e070aa8199973f9
Amir P <apourrezaei@gmail.com>
2019-05-01 18:35:34 -0400

    Survey Question 1

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,5 +4,9 @@ module.exports = function(app) {
     app.get("/api/friends", function(req,res){
         res.json(friends);
     });
-    
+
+    app.post("/api/friends", function(req, res) {
+        console.log(req.body.scores);
+        
+    });
 }
