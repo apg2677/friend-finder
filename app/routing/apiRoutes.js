68ee5714e7e7c0778ac71fcf0d38b36271f83bb3
Amir P <apourrezaei@gmail.com>
2019-05-01 19:20:19 -0400

    Diff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,21 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        console.log(req.body);
+        var user = req.body;
+
+        var diff=0;
+        console.log(user);
+
+        for (var i in user.scores) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        for(var i in user.scores) {
+            let tempDiff = user.scores[i] - friends[0].scores[i];
+            diff += Math.abs(tempDiff);
+        }
+        console.log("Diff: " + diff);
+
 
     });
 }
